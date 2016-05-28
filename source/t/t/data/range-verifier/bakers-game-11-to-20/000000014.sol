963a07a342575ad4b2452bba461cad4c4c10f0ce
Shlomi Fish <shlomif@shlomifish.org>
2016-05-28 13:57:01 +0300

    Remove some trailing space.

--- source/t/t/data/range-verifier/bakers-game-11-to-20/000000014.sol
+++
@@ -1,3 +0,0 @@
-I could not solve this game.
-Total number of states checked is 1891.
-This scan generated 1891 states.
--- source/t/t/data/range-verifier/bakers-game-solutions-dir/000000010.sol
+++
@@ -1,3 +0,0 @@
-I could not solve this game.
-Total number of states checked is 3436.
-This scan generated 3436 states.
